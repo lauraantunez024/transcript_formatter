d42a76646b9d642363a5b50bcd951ad4af79bfec
Laura Antunez <lauraantunez024@gmail.com>
2025-01-17 14:59:36 -0500

    adding debug

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def format_text():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
 
 
